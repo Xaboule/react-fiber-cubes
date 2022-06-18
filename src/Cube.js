c041ef16839901c59377c706cfb7a569540e7861
Romain MURA <romain@mura.io>
2022-06-18 19:05:49 +0200

    add keypress watching test

--- src/Cube.js
+++ src/Cube.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
-import { useSpring, a } from "@react-spring/three";
-import { useControls } from './useControls'
+import { useState, useEffect } from 'react';
+import { useSpring, a } from '@react-spring/three';
+import { useControls } from './useControls';
+import { useFrame } from '@react-three/fiber';
 export default function Cube(props) {
-  
-  const controls = useControls()
-
+  const controls = useControls();
+  const { position } = props;
+  const [cubePos, setCubePos] = useState(position);
   const [active, setActive] = useState(false);
   const [hovered, setHovered] = useState(false);
   const { scale, color } = useSpring({
@@ -12,14 +13,20 @@ export default function Cube(props) {
     color: active ? `#66ffff` : `#ffff66`,
   });
   const delay = 4;
-  useEffect(
-    () => {
-      setTimeout(() => setActive(false), delay * 1000)},
-    );
-      // clears timeout before running the new effect
+  useFrame(() => {
+    if (controls.current.forward) {
+      setCubePos([cubePos[0], cubePos[1], cubePos[2] + 0.1]);
+    } else if (controls.current.backward) {
+      setCubePos([cubePos[0], cubePos[1], cubePos[2] - 0.1]);
+    }
+  });
+  useEffect(() => {
+    setTimeout(() => setActive(false), delay * 1000);
+  }, []);
+  // clears timeout before running the new effect
   return (
     <a.mesh
-      {...props}
+      position={cubePos}
       scale={scale}
       onClick={() => setActive(!active)}
       onPointerOver={() => setHovered(true)}
